2c3aabe8b28f08257dfe070b9d23e0bfe1b2b37f
Denis Olehov <denolehov@gmail.com>
2020-10-28 03:01:15 +0200

    feat: Paste URLs into selection "notion style"

---
+++ main.ts
@@ -0,0 +1,39 @@
+import { Plugin } from "obsidian";
+import { clipboard } from "electron";
+
+export default class UrlIntoSelection extends Plugin {
+  async onload() {
+    console.log(this.app);
+    this.addCommand({
+      id: "paste-url-into-selection",
+      name: "",
+      callback: () => this.urlIntoSelection(),
+      hotkeys: [
+        {
+          modifiers: ["Mod", "Shift"],
+          key: "v",
+        },
+      ],
+    });
+  }
+
+  urlIntoSelection(): void {
+    let activeLeaf: any = this.app.workspace.activeLeaf;
+    let editor = activeLeaf.view.sourceMode.cmEditor;
+    let selectedText = editor.somethingSelected()
+      ? editor.getSelection()
+      : false;
+    let clipboardText = clipboard.readText("clipboard");
+
+    if (selectedText && clipboardText && this.isUrl(clipboardText)) {
+      editor.replaceSelection(`[${selectedText}](${clipboardText})`);
+    }
+  }
+
+  isUrl(text: string): boolean {
+    let urlRegex = new RegExp(
+      "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$"
+    );
+    return urlRegex.test(text);
+  }
+}
