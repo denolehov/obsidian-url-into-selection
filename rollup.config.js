2f5a47be4ed7f6b2aa2b2799043c2b1a3586e92f
AidenLx <aiden.lx@outlook.com>
2021-04-24 23:08:02 +0800

    build: add nodePolyfills plugin for mobile compatibility

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import nodePolyfills from "rollup-plugin-node-polyfills";
 
 export default {
   input: "src/main.ts",
@@ -11,5 +12,5 @@ export default {
     exports: "default",
   },
   external: ["obsidian"],
-  plugins: [typescript(), nodeResolve({ browser: true }), commonjs()],
+  plugins: [typescript(), nodePolyfills(), nodeResolve({ browser: true }), commonjs()],
 };
